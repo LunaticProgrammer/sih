edb27f7e2f8b0f0577e98277a1903c08cae3ab76
varnit <varnitsharma.102@gmail.com>
2019-02-16 16:19:24 +0530

    new commit

---
+++ Medichain/src/app/login-options/login-options.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoginOptionsPage } from './login-options.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginOptionsPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [LoginOptionsPage]
+})
+export class LoginOptionsPageModule {}
